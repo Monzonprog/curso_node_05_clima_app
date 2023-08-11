bba1a915417c51f6bdf32548e8bad25981259b2b
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-08-11 21:55:42 +0200

    Tercer video

---
+++ models/busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas{
+    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+    constructor(){
+        //TODO: leer DB si no existe
+    }
+
+    async ciudad(lugar= ''){
+        //petición http
+        console.log(lugar);
+
+        return [];//Retornará las ciudades que coincidan
+    }
+}
+
+module.exports = Busquedas;

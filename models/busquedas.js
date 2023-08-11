c8b62fa1807d219c07545caea465eac02c79696c
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-08-11 22:23:19 +0200

    Cuarto video

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,15 +1,23 @@
-class Busquedas{
+const axios = require('axios');
+
+class Busquedas {
     historial = ['Tegucigalpa', 'Madrid', 'San José'];
 
-    constructor(){
+    constructor() {
         //TODO: leer DB si no existe
     }
 
-    async ciudad(lugar= ''){
-        //petición http
-        console.log(lugar);
+    async ciudad(lugar = '') {
+        try {
+            //petición http
+            //console.log('Ciudad:' , lugar);
+            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            console.log(resp.data);
 
-        return [];//Retornará las ciudades que coincidan
+            return []; //Retornará las ciudades que coincidan
+        } catch (error) {
+            return [];
+        }
     }
 }
 
